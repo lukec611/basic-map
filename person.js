1264ba74dea1ae726669613f573fc0f71a2ddb37
luke lincoln <lukes611@gmail.com>
2019-04-14 17:36:58 +1000

    fixing person's dimensions

--- person.js
+++ person.js
@@ -23,6 +23,21 @@ class Person {
         
         const halfBodyWidth = bodyWidth * 0.5;
 
+        this.dimensions = {
+            headSize,
+            bodyWidth,
+            bodyThickness,
+            torsoHeight,
+            legHeight,
+            legThickness,
+            chestSize,
+            armHeight,
+            armThickness,
+            armGap,
+            legGap,
+            halfBodyWidth,
+        };
+
         this.cubes.head = new Cube({
             x: 0,
             y:  halfBodyWidth - headSize,
@@ -100,7 +115,7 @@ class Person {
             container.appendChild(this.cubes[key].element);
         });
         this.container = container;
-        this.container.style.transformOrigin = `2.5px 13px 0px`;
+        this.container.style.transformOrigin = `${bodyThickness/2}px ${bodyWidth/2}px 0px`;
     }
 
     // i ranges 0-1
@@ -115,13 +130,14 @@ class Person {
         var deg = Math.sin(i * 2 * Math.PI) * 8;
         var degL = Math.sin(i * 2 * Math.PI) * 10;
         la.style.transform = `rotateY(${deg}deg)`;
-        la.style.transformOrigin = '10px 0px 55px';
+        const neckHeight = this.dimensions.legHeight + this.dimensions.torsoHeight;
+        la.style.transformOrigin = `${this.dimensions.bodyThickness/2}px 0px ${neckHeight}px`;
         ra.style.transform = `rotateY(${-deg}deg)`;
-        ra.style.transformOrigin = '10px 0px 55px';
+        ra.style.transformOrigin = `${this.dimensions.bodyThickness/2}px 0px ${neckHeight}px`;
         ll.style.transform = `rotateY(${-degL}deg)`;
-        ll.style.transformOrigin = '10px 0px 55px';
+        ll.style.transformOrigin = `${this.dimensions.bodyThickness/2}px 0px ${this.dimensions.legHeight}px`;
         rl.style.transform = `rotateY(${degL}deg)`;
-        rl.style.transformOrigin = '10px 0px 55px';
+        rl.style.transformOrigin = `${this.dimensions.bodyThickness/2}px 0px ${this.dimensions.legHeight}px`;
     }
 
     
